3cc47d26656fadde69867febb206a1ca9a533095
Insidious Echo <thestkr007@gmail.com>
2026-06-23 18:04:29 +0000

    Fixed some bugs

--- scanner.py
+++ scanner.py
@@ -8,7 +8,7 @@ print("Select the type of scan you want")
 print("1. Fast Scan (Scan top 10 most common ports)")
 print("2. Deep Scan (Scan a range of ports (1, 1025))")
 
-choice = input("Enter choice (1 or 2): )
+choice = input("Enter choice (1 or 2): ")
 if choice == "1":
     ports_to_scan = [21, 22, 23, 25, 53, 80, 110, 139, 443, 445]
 elif choice == "2":
@@ -21,7 +21,7 @@ for ip in host_list:
     for port in ports_to_scan:  
         s = socket.socket()
         s.settimeout(1.0)
-            print(f"Scanning {ip} on port {port}")
+        print(f"Scanning {ip} on port {port}")
         result = s.connect_ex((str(ip), port))
         if result == 0:
-        print(f"Host {ip} {port} is active!")
+            print(f"Host {ip} {port} is active!")
